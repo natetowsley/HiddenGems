38ac0dde24129f340a66722734499601099cd8dc
natetowsley <natetowsley@gmail.com>
2026-05-28 11:01:33 -0700

    feat: added ReviewService & recalculation of avg_rating to LocationRepository

--- apps/api/src/main/java/com/hiddengems/api/repository/LocationRepository.java
+++ apps/api/src/main/java/com/hiddengems/api/repository/LocationRepository.java
@@ -2,6 +2,9 @@ package com.hiddengems.api.repository;
 
 import com.hiddengems.api.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +19,7 @@ public interface LocationRepository extends JpaRepository<Location, UUID> {
 
     List<Location> findByCreatedBy(UUID createdBy);
 
+    @Modifying
+    @Query("UPDATE Location l SET l.avgRating = COALESCE((SELECT AVG(r.rating) FROM Review r WHERE r.locationId = :locationId), 0) WHERE l.id = :locationId")
+    void recalculateAvgRating(@Param("locationId") UUID locationId);
 }
